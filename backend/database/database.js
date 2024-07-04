a0e41cb7292184fd9a43134c87b1b33e2650b097
imwaltdiaz <walter@waltdiaz.com>
2024-07-03 19:45:16 -0500

    fixed db.js

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,19 @@
 import { Sequelize } from "sequelize";
 
+// Configuración de Sequelize
 export const sequelize = new Sequelize("postgres", "postgres", "postgres", {
-    host: "localhost",
-    dialect: "postgres"
+    host: "127.0.0.1",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
 });
+
+// Función para autenticar la conexión
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexión exitosa a la base de datos.');
+    } catch (error) {
+        console.error('No se puede conectar a la base de datos:', error);
+    }
+})();
