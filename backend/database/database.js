97d63cc4b1134f28f7ae1dbeedb2744eea83d880
imwaltdiaz <walter@waltdiaz.com>
2024-07-07 20:46:56 -0500

    fixed db21

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,15 +1,18 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+export const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
     dialect: "postgres",
+    port: process.env.DB_PORT,
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Es posible que necesites esto dependiendo de la configuración de SSL de tu servidor
-        }
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-});
+  }
+);
