f20a79947ffac87a8e9953ad04637c525f3b5d5d
imwaltdiaz <walter@waltdiaz.com>
2024-07-07 21:09:11 -0500

    fixed db25

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,18 +1,12 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+export const sequelize = new Sequelize("postgres", "postgres@deploybeta0postgres", "Database!1", {
+    host: "deploybeta0postgres.postgres.database.azure.com",
     dialect: "postgres",
-    port: process.env.DB_PORT,
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-  }
-);
+});
