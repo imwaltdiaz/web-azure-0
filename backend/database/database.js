a3d094763e9125fb53173995b73ba0327e334c6e
imwaltdiaz <walter@waltdiaz.com>
2024-07-07 20:12:08 -0500

    prueba backend y db en azure

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("postgres", "postgres", "postgres", {
-    host: "localhost",
-    dialect: "postgres"
+export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
+    }
 });
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import { sequelize } from "./database/database.js";
 import { Usuario } from "./models/Usuario.js";
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ 
     extended: true
 }));
+
 /////////////CONEXION////////////////
 async function verificarConexion() {
   try {
